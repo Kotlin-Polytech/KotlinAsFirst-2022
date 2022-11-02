26b548c4adc3e5afd196c25f3939dbad4e76eb43
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-11-02 20:14:28 +0300

    remove random unicode chars from UnsignedBigIntegerTest.kt

--- test/lesson11/task1/UnsignedBigIntegerTest.kt
+++ test/lesson11/task1/UnsignedBigIntegerTest.kt
@@ -30,7 +30,7 @@ internal class UnsignedBigIntegerTest {
     fun ubiTimes() {
         assertEquals(
             UnsignedBigInteger("18446744073709551616"),
-            UnsignedBigInteger("4294967296‬") * UnsignedBigInteger("4294967296‬")
+            UnsignedBigInteger("4294967296") * UnsignedBigInteger("4294967296")
         )
     }
 
@@ -38,8 +38,8 @@ internal class UnsignedBigIntegerTest {
     @Tag("16")
     fun ubiDiv() {
         assertEquals(
-            UnsignedBigInteger("4294967296‬"),
-            UnsignedBigInteger("18446744073709551616") / UnsignedBigInteger("4294967296‬")
+            UnsignedBigInteger("4294967296"),
+            UnsignedBigInteger("18446744073709551616") / UnsignedBigInteger("4294967296")
         )
     }
 
@@ -49,7 +49,7 @@ internal class UnsignedBigIntegerTest {
         assertEquals(UnsignedBigInteger(5), UnsignedBigInteger(19) % UnsignedBigInteger(7))
         assertEquals(
             UnsignedBigInteger(0),
-            UnsignedBigInteger("18446744073709551616") % UnsignedBigInteger("4294967296‬")
+            UnsignedBigInteger("18446744073709551616") % UnsignedBigInteger("4294967296")
         )
     }
 
